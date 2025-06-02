a4fbcef5cf853f6e8b06248155ad16b850c419ed
theusll <rm95316@estudante.fieb.edu.br>
2025-06-02 10:47:50 +0000

    Grid

--- src/PokeGrid.jsx
+++ src/PokeGrid.jsx
@@ -1,8 +1,6 @@
 import poke from './assets/pokeapi_256.png'
 import eevee from './assets/eevee.jpg'
-import '<div className="" />'
 import './PokeGrid.css'
-<PokeGrid></PokeGrid>
 const PokeGrid = () => {
     return (
         <div>
